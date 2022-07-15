271e6eef2e3706e1b14bfc168744ede5f7664eab
yulatne <tyzhina@icloud.com>
2022-07-16 00:10:19 +0300

    FreeMiles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 20888;
+        int bonusMiles = 1;
+        int rubles = 20;
+        int totalBonus = price * bonusMiles / rubles;
+        System.out.println(totalBonus);
+    }
+}
